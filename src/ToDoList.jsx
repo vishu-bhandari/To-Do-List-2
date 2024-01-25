f17a05a0f0f9c653ff616d963aa95f88b098708c
vishu bhandari <deevishu1234@gmail.com>
2024-01-25 22:21:53 +0530

    added new items

--- src/ToDoList.jsx
+++ src/ToDoList.jsx
@@ -1,42 +1,44 @@
 import React from "react";
-import "./index.css"
-import Button from '@mui/material/Button';
-import AddIcon from '@mui/icons-material/Add';
+import "./index.css";
+import Button from "@mui/material/Button";
+import AddIcon from "@mui/icons-material/Add";
 import { useState } from "react";
 
-const ToDoList=()=>{
+const ToDoList = () => {
+  const [item, setItem] = useState("buy an apple");
+  const [newitem, setNewItem] = useState([]);
 
-    const [item,setItem]=useState('buy an apple');
-    const [newitem, setNewItem]=useState([]);
+  const itemEvent = (event) => {
+    setItem(event.target.value);
+  };
 
-
-    const itemEvent=(event)=>{
-        setItem(event.target.value);
-    }
-    return (
-        <>
-        <div className="main_div">
-            <div className="center_div">
-            <br />
-            <h1>ToDo List</h1>
-            <br />
-            <input type="text" placeholder="Add An Item" 
-            onChange={itemEvent} />
-            <Button className="new_btn"> 
-            <AddIcon/>
-            </Button>
-             <br />
-             <ol>
-                <li>
-                {item}
-                </li>
-                newitem.map()
-             </ol>
-             <br /> 
-            </div>
+  const listOfItems=()=>{
+    setNewItem((prevValue)=>{
+        return [...prevValue,item]
+    });
+  }
+  return (
+    <>
+      <div className="main_div">
+        <div className="center_div">
+          <br />
+          <h1>ToDo List</h1>
+          <br />
+          <input type="text" placeholder="Add An Item" onChange={itemEvent} />
+          <Button className="new_btn " onClick={listOfItems}>
+            <AddIcon />
+          </Button>
+          <br />
+          <ol>
+             {newitem.map((val) => {
+              return <li>{val}</li>;
+            })}
+          </ol>
+          <br />
         </div>
-        </>
-    )
-}
+      </div>
+    </>
+  );
+};
 
 export default ToDoList;
