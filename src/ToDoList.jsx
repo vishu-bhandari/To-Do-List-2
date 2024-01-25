993ff49746b6d661d27928240f46bde8c04eac48
vishu bhandari <deevishu1234@gmail.com>
2024-01-25 22:05:16 +0530

    c

--- src/ToDoList.jsx
+++ src/ToDoList.jsx
@@ -1,9 +1,40 @@
 import React from "react";
+import "./index.css"
+import Button from '@mui/material/Button';
+import AddIcon from '@mui/icons-material/Add';
+import { useState } from "react";
 
 const ToDoList=()=>{
+
+    const [item,setItem]=useState('buy an apple');
+    const [newitem, setNewItem]=useState([]);
+
+
+    const itemEvent=(event)=>{
+        setItem(event.target.value);
+    }
     return (
         <>
-
+        <div className="main_div">
+            <div className="center_div">
+            <br />
+            <h1>ToDo List</h1>
+            <br />
+            <input type="text" placeholder="Add An Item" 
+            onChange={itemEvent} />
+            <Button className="new_btn"> 
+            <AddIcon/>
+            </Button>
+             <br />
+             <ol>
+                <li>
+                {item}
+                </li>
+                newitem.map()
+             </ol>
+             <br /> 
+            </div>
+        </div>
         </>
     )
 }
